eab005b0857c3606df708c58dbffd4ce9a5f2fcb
SankyPatil <sp0026720@gmail.com>
2025-08-04 19:04:14 +0530

    Update name in Navbar from Sanket to Sanky

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         {/* Logo */}
         <div className="text-lg font-semibold cursor-pointer">
           <span className="text-[#8245ec]">&lt;</span>
-          <span className="text-white">Sanket</span>
+          <span className="text-white">Sanky</span>
           <span className="text-[#8245ec]">/</span>
           <span className="text-white">Patil</span>
           <span className="text-[#8245ec]">&gt;</span>
